dd6cca894ca55c5022fa74e3531c47ed4435b8ab
endoverdosing <endoverdosing@gmail.com>
2026-05-19 19:06:53 -0700

    Updated

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -530,6 +530,26 @@ function play(raw, skipProxy, videoId) {
     var subtitleText = document.getElementById('subtitle-text');
     var tooltip = document.getElementById('tooltip');
 
+    var volSlider = document.getElementById('volume-slider');
+    var volIcon = document.getElementById('volume-icon');
+    var VOL_ICONS = {
+        off: '<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 640 512"><path fill="currentColor" d="M301.1 34.8C312.6 40 320 51.4 320 64V448c0 12.6-7.4 24-18.9 29.2s-25 3.1-34.4-5.3L131.8 352H64c-35.3 0-64-28.7-64-64V224c0-35.3 28.7-64 64-64h67.8L266.7 40.1c9.4-8.4 22.9-10.4 34.4-5.3zM425 167l55 55 55-55c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-55 55 55 55c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-55-55-55 55c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l55-55-55-55c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0z"/></svg>',
+        low: '<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 640 512"><path fill="currentColor" d="M301.1 34.8C312.6 40 320 51.4 320 64V448c0 12.6-7.4 24-18.9 29.2s-25 3.1-34.4-5.3L131.8 352H64c-35.3 0-64-28.7-64-64V224c0-35.3 28.7-64 64-64h67.8L266.7 40.1c9.4-8.4 22.9-10.4 34.4-5.3zm105.5 145.2C434.1 199.1 448 225.9 448 256s-13.9 56.9-35.4 74.5c-10.3 8.4-25.4 6.8-33.8-3.5s-6.8-25.4 3.5-33.8C393.1 284.4 400 271 400 256s-6.9-28.4-17.7-37.3c-10.3-8.4-11.8-23.5-3.5-33.8s23.5-11.8 33.8-3.5z"/></svg>',
+        mid: '<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 640 512"><path fill="currentColor" d="M473.1 107c43.2 35.2 70.9 88.9 70.9 149s-27.7 113.8-70.9 149c-10.3 8.4-25.4 6.8-33.8-3.5s-6.8-25.4 3.5-33.8C475.3 341.3 496 301.1 496 256s-20.7-85.3-53.2-111.8c-10.3-8.4-11.8-23.5-3.5-33.8s23.5-11.8 33.8-3.5zm-60.5 74.5C434.1 199.1 448 225.9 448 256s-13.9 56.9-35.4 74.5c-10.3 8.4-25.4 6.8-33.8-3.5s-6.8-25.4 3.5-33.8C393.1 284.4 400 271 400 256s-6.9-28.4-17.7-37.3c-10.3-8.4-11.8-23.5-3.5-33.8s23.5-11.8 33.8-3.5zM301.1 34.8C312.6 40 320 51.4 320 64V448c0 12.6-7.4 24-18.9 29.2s-25 3.1-34.4-5.3L131.8 352H64c-35.3 0-64-28.7-64-64V224c0-35.3 28.7-64 64-64h67.8L266.7 40.1c9.4-8.4 22.9-10.4 34.4-5.3z"/></svg>',
+        high: '<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 640 512"><path fill="currentColor" d="M533.6 32.5C598.5 85.3 640 165.8 640 256s-41.5 170.8-106.4 223.5c-10.3 8.4-25.4 6.8-33.8-3.5s-6.8-25.4 3.5-33.8C557.5 398.2 592 331.2 592 256s-34.5-142.2-88.7-186.3c-10.3-8.4-11.8-23.5-3.5-33.8s23.5-11.8 33.8-3.5zM473.1 107c43.2 35.2 70.9 88.9 70.9 149s-27.7 113.8-70.9 149c-10.3 8.4-25.4 6.8-33.8-3.5s-6.8-25.4 3.5-33.8C475.3 341.3 496 301.1 496 256s-20.7-85.3-53.2-111.8c-10.3-8.4-11.8-23.5-3.5-33.8s23.5-11.8 33.8-3.5zm-60.5 74.5C434.1 199.1 448 225.9 448 256s-13.9 56.9-35.4 74.5c-10.3 8.4-25.4 6.8-33.8-3.5s-6.8-25.4 3.5-33.8C393.1 284.4 400 271 400 256s-6.9-28.4-17.7-37.3c-10.3-8.4-11.8-23.5-3.5-33.8s23.5-11.8 33.8-3.5zM301.1 34.8C312.6 40 320 51.4 320 64V448c0 12.6-7.4 24-18.9 29.2s-25 3.1-34.4-5.3L131.8 352H64c-35.3 0-64-28.7-64-64V224c0-35.3 28.7-64 64-64h67.8L266.7 40.1c9.4-8.4 22.9-10.4 34.4-5.3z"/></svg>'
+    };
+    function updateVolumeUI() {
+        var vol = v.muted ? 0 : v.volume;
+        if (volSlider) volSlider.value = Math.round(vol * 100);
+        if (volIcon) {
+            if (vol === 0) volIcon.innerHTML = VOL_ICONS.off;
+            else if (vol < 0.25) volIcon.innerHTML = VOL_ICONS.low;
+            else if (vol < 0.6) volIcon.innerHTML = VOL_ICONS.mid;
+            else volIcon.innerHTML = VOL_ICONS.high;
+        }
+    }
+    updateVolumeUI();
+
     var hideTimer = null;
     var dragging = false;
     var shown = false;
@@ -924,6 +944,29 @@ function play(raw, skipProxy, videoId) {
         scheduleRetry();
         attemptAutoplay();
         restoreVolume();
+
+        if (volIcon) volIcon.innerHTML = VOL_ICONS.high;
+        if (volSlider) volSlider.value = 100;
+        updateVolumeUI();
+
+        if (volSlider) {
+            volSlider.addEventListener('input', function () {
+                v.volume = this.value / 100;
+                v.muted = this.value == 0;
+                updateVolumeUI();
+            });
+        }
+
+        var btnVolume = document.getElementById('btn-volume');
+        if (btnVolume) {
+            btnVolume.addEventListener('click', function (e) {
+                e.stopPropagation();
+                v.muted = !v.muted;
+                updateVolumeUI();
+            });
+        }
+
+        v.addEventListener('volumechange', updateVolumeUI);
     }
 
     var _autoplayUnlocked = false;
